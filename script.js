52f334361ef65668d8c6bf48575c5ee390aa6ce7
Luka <luka.luka.luka93@gmail.com>
2024-09-19 12:45:03 -0700

    created speec Recognition variables, and then made a function thats gonna itterate the transcript of words I say, erased white space and made it all lower case, made an array of colors

---
+++ script.js
@@ -0,0 +1,28 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const rec = new SpeechRecognition();
+
+rec.lang = 'en-US';
+rec.continuous = false;
+
+rec.onresult = function (e) {
+    const acceptedColors = [
+        'red',
+        'blue',
+        'green',
+        'yellow',
+        'pink',
+        'brown',
+        'purple',
+        'orange',
+        'black',
+        'white',
+    ]
+    // console.log(e.results[0][0].transcript) - it will give me the word I said, always the first one, ex. blue
+    // Thats why we have to loop through it, and show each word one after another
+    for (let i = e.resultIndex; i < e.results.length; i++) {
+        const script = e.results[i][0].transcript.toLowerCase().trim(); // all lower case and no white space
+        console.log(script)
+    }
+}
+rec.start();
